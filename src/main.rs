ae499941a32b2bc0ce066cca4068a559e8c24b32
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2024-12-26 12:51:39 -0500

    Moved to clap for command line arguments. Added output to file option

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ use std::error::Error;
 use std::fs::File;
 use serde::{Deserialize, Serialize};
 use flate2::read::GzDecoder;
-use std::io::{Read, BufReader, BufRead, stdout};
+use std::io::{Read, BufReader, BufRead, Write, stdout};
 use std::collections::HashSet;
 use std::env::args;
+use clap::Parser;
 
-fn parse_genelist(genelist_filename: &String) -> HashSet<String> {
+fn parse_genelist(genelist_filename: String) -> HashSet<String> {
     let mut gene_set = HashSet::new();
     let file = File::open(genelist_filename).expect("Failed to open genelist");
     let reader = BufReader::new(file);
@@ -73,18 +74,49 @@ struct ClinVarRecord {
 //    ReviewStatusOncogenicity: String,
 }
 
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// ClinVar Variant TSV
+    #[arg(long, short='i', required=true)]
+    input: String,
+
+    /// Optional filename for list of genes to filter on
+    #[arg(long, short='l')]
+    genelist: Option<String>,
+
+    /// Output filename. If not provided will print to STDOUT.
+    #[arg(long, short='o')]
+    output: Option<String>,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = args().collect();
-    let genelist_filename: &String = &args[1];
-    let gene_set = parse_genelist(genelist_filename);
-    let filename = "variant_summary.txt.gz";
-    let file = File::open(filename).expect("Failed to open input file");
+    let cli = Cli::parse();
+    let filename = cli.input;
+    let mut use_gene_list = false;
+    let mut gene_set = HashSet::new();
+    if let Some(genelist_filename) = cli.genelist {
+        gene_set = parse_genelist(genelist_filename);
+        use_gene_list = true;
+    }
+    let file = File::open(filename.clone()).expect("Failed to open input file");
     let reader_box: Box<dyn Read> = if filename.ends_with(".gz") {Box::new(GzDecoder::new(file))} else {Box::new(file)};
     let mut tsv_reader = csv::ReaderBuilder::new().delimiter(b'\t').from_reader(reader_box); 
-    let mut wtr = csv::WriterBuilder::new().delimiter(b'\t').from_writer(stdout());
+    let writer: Box<dyn Write> = match cli.output {
+        Some(filename) => {
+            let file = File::create(filename)?;
+            Box::new(file)
+        }
+        None => Box::new(stdout().lock()),
+    };
+
+    let mut wtr = csv::WriterBuilder::new().delimiter(b'\t').from_writer(writer);
     for result in tsv_reader.deserialize() {
         let mut record: ClinVarRecord = result?;
-        if gene_set.contains(&record.GeneSymbol) && record.Assembly == "GRCh38" {
+          if use_gene_list && !gene_set.contains(&record.GeneSymbol) {
+              continue;
+          }
+          if record.Assembly == "GRCh38" {
               if record.Name.contains("c.") {
                   let rec_name_fields: Vec<&str> = record.Name.split(":").collect();
 
